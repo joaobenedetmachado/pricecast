0b2b686a0df4d150ba846493c4cd1b94c86e5da3
joao machado <jvbm2177@gmail.com>
2025-11-26 15:13:31 -0300

    starting api

---
+++ api/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, File, UploadFile
+import pandas as pd 
+from datetime import date, timedelta
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
--- models/main.py
+++ models/main.py
@@ -1,10 +1,17 @@
 from ml.predict import return_predictions
 import pandas as pd
 csv_path = "/home/joao/pricecast/scraper/data/Ethereumprice.csv"
-df = pd.read_csv(csv_path)
-df["price"] = df["price"].str.replace("$", "", regex=False).str.replace(",", "", regex=False).astype(float)
 
-predictions = return_predictions(df, days_to_predict=7, window_size=30)
+def predict_directive(csv_path, days, window_size):
+    df = pd.read_csv(csv_path)
+    df["price"] = df["price"].str.replace("$", "", regex=False).str.replace(",", "", regex=False).astype(float)
 
-for p in predictions:
-    print(p)
+    predictions = return_predictions(df, days_to_predict=days, window_size=window_size)
+
+    result = []
+    for p in predictions:
+        result.append(p)
+    
+    return result
+
+print(predict_directive(csv_path, 7, 30))
